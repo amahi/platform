cfcd0c01525c32a70b995b577e3e818b7bf3f5e1
Bogdan Mitrea <bgm@server.fake>
2012-11-30 17:03:47 -0500

    Few more tests for the users tab

--- spec/requests/users_tab_spec.rb
+++ spec/requests/users_tab_spec.rb
@@ -23,7 +23,7 @@ describe "Users tab" do
 		expect(page).to have_content("newuser")
 		expect(page).to have_content("fullname")
 	end
-	it "should allow an admin user to delete another user", :js => true do
+	it "should allow an admin user to delete a regular user", :js => true do
 		admin = create(:admin)
 		user = create(:user)
 		visit root_path
@@ -35,29 +35,72 @@ describe "Users tab" do
         visit users_path
 		expect(page).to have_content("Username")
 		expect(page).to have_content("Full Name")
-		expect(page).to have_content(user.login)
 		click_link user.login
-#		expect(page).to have_content("Delete user "+user.login)
+		expect(page).to have_selector("span.delete", :visible => true, :text => user.login);
 		click_link "Delete"
 		expect(page).to have_no_content(user.login)
 	end
 	it "should not allow an admin user to delete its own account" do
-		pending
+		admin = create(:admin)
+		visit root_path
+		expect(page).to have_content("Amahi Server Login")
+        fill_in "username", :with => admin.login
+        fill_in "password", :with => "secret"
+        click_button "Log In"
+		expect(page).to have_content("Setup")
+        visit users_path
+		expect(page).to have_content("Username")
+		expect(page).to have_content("Full Name")
+		click_link admin.login
+		expect(page).to have_no_selector("span.delete",:visible => true, :text => admin.login);
 	end
 	it "should not allow an admin user to revoke its own admin rights" do
-		pending
-	end
-	it "should allow an admin user to create a regular user" do
-		pending
-	end
-	it "should allow an admin nuser to create an admin user" do
-		pending
+		admin = create(:admin)
+		visit root_path
+		expect(page).to have_content("Amahi Server Login")
+        fill_in "username", :with => admin.login
+        fill_in "password", :with => "secret"
+        click_button "Log In"
+		expect(page).to have_content("Setup")
+        visit users_path
+		expect(page).to have_content("Username")
+		expect(page).to have_content("Full Name")
+		click_link admin.login
+		expect(page.find_by_id("checkbox-user_admin_1")[:disabled]).to eq 'disabled'
 	end
 	it "should allow an admin user to revoke admin rights to another user" do
-		pending
+		admin = create(:admin)
+		user = create(:admin)
+		visit root_path
+		expect(page).to have_content("Amahi Server Login")
+        fill_in "username", :with => admin.login
+        fill_in "password", :with => "secret"
+        click_button "Log In"
+		expect(page).to have_content("Setup")
+        visit users_path
+		expect(page).to have_content("Username")
+		expect(page).to have_content("Full Name")
+		click_link user.login
+		expect(page).to have_checked_field("checkbox-user_admin_2")
+		uncheck("checkbox-user_admin_2");
+		expect(page).to have_unchecked_field("checkbox-user_admin_2")
 	end
 	it "should allow an admin user to promote a regular user to admin" do
-		pending
+		admin = create(:admin)
+		user = create(:user)
+		visit root_path
+		expect(page).to have_content("Amahi Server Login")
+        fill_in "username", :with => admin.login
+        fill_in "password", :with => "secret"
+        click_button "Log In"
+		expect(page).to have_content("Setup")
+        visit users_path
+		expect(page).to have_content("Username")
+		expect(page).to have_content("Full Name")
+		click_link user.login
+		expect(page).to have_unchecked_field("checkbox-user_admin_2")
+		check("checkbox-user_admin_2");
+		expect(page).to have_checked_field("checkbox-user_admin_2")
 	end 
 	it "should allow an admin user to change his full name" do
 		pending
