2d49c4e1f1677c45e2510e1d0e3e0e0f8e6964ed
Carlos Puchol <cpg+git@amahi.org>
2013-02-16 18:24:34 -0800

    make tmpdir something that can be used in development on other OSs

--- config/initializers/tmpdir.rb
+++ config/initializers/tmpdir.rb
@@ -1,2 +1,6 @@
-# temp dir for our own use -- in F18, /tmp is "chrooted" for apache for security
-TMPDIR = '/var/hda/tmp'
+if File.exists?('/var/hda/tmp')
+	# temp dir for our own use -- in F18, /tmp is "chrooted" for apache for security
+	TMPDIR = '/var/hda/tmp'
+else
+	TMPDIR = File.join(Rails.root, 'tmp')
+end
