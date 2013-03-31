88663cc9f6876bc9873e2d392d303e64abf0c40d
Carlos Puchol <cpg+git@amahi.org>
2013-03-31 13:59:31 -0700

    re-indent disk_utils file

--- plugins/002-disks/lib/disks/disk_utils.rb
+++ plugins/002-disks/lib/disks/disk_utils.rb
@@ -18,73 +18,73 @@ require 'socket'      # Sockets are in standard library
 
 class DiskUtils
 
-  # return information on hdd temperature - requires hddtemp service running!
-  class << self
-        def stats
-      host = 'localhost'
-      port = 7634
+	# return information on hdd temperature - requires hddtemp service running!
+	class << self
+		def stats
+			host = 'localhost'
+			port = 7634
 
-      begin
-        s = TCPSocket.open(host, port)
-      rescue
-        return []
-      end
+			begin
+				s = TCPSocket.open(host, port)
+			rescue
+				return []
+			end
 
-      res = ''
-      while line = s.gets   # Read lines from the socket
-        res += line.chop      # And print with platform line terminator
-      end
-      s.close               # Close the socket when done
+			res = ''
+			while line = s.gets   # Read lines from the socket
+				res += line.chop      # And print with platform line terminator
+			end
+			s.close               # Close the socket when done
 
-      disks = res.split '||'
+			disks = res.split '||'
 
-      res = []
-      disks.each do |disk|
-        # split the info and do cleanup
-        i = disk.gsub(/^\||\|$/, '').split('|')
-        model = i[1].gsub(/[^A-Za-z0-9\-_\s\.]/, '') rescue "Unkown"
-        next if model == '???'
-        t = (i[2] =~ /(^\*$)|nos|err/i) ? nil : i[2].to_i
-        tempcolor = "cool"
-        celsius = "-"
-        farenheight = "-"
-        if t > 0
-          celsius = t.to_s
-          tempcolor = "warm" if t > 39
-          tempcolor = "hot" if t > 49
-          farenheight = (t * 1.8 + 32).to_i.to_s
-        end
-        d = Hash.new
-        d[:device] = i[0]
-        d[:model] = model
-        d[:temp_c] = celsius
-        d[:temp_f] = farenheight
-        d[:tempcolor] = tempcolor
-        res.push(d)
-      end
-      res
-    end
+			res = []
+			disks.each do |disk|
+				# split the info and do cleanup
+				i = disk.gsub(/^\||\|$/, '').split('|')
+				model = i[1].gsub(/[^A-Za-z0-9\-_\s\.]/, '') rescue "Unkown"
+				next if model == '???'
+				t = (i[2] =~ /(^\*$)|nos|err/i) ? nil : i[2].to_i
+				tempcolor = "cool"
+				celsius = "-"
+				farenheight = "-"
+				if t > 0
+					celsius = t.to_s
+					tempcolor = "warm" if t > 39
+					tempcolor = "hot" if t > 49
+					farenheight = (t * 1.8 + 32).to_i.to_s
+				end
+				d = Hash.new
+				d[:device] = i[0]
+				d[:model] = model
+				d[:temp_c] = celsius
+				d[:temp_f] = farenheight
+				d[:tempcolor] = tempcolor
+				res.push(d)
+			end
+			res
+		end
 
-    def mounts
-      s = `df`.split( /\r?\n/ )[1..-1] || ["","Incorrect data returned"]
+		def mounts
+			s = `df`.split( /\r?\n/ )[1..-1] || ["","Incorrect data returned"]
 
-      mount = []
-      res = []
-      s.each do |line|
-        word = line.split(/\s+/)
-        mount.push(word)
-      end
-      mount.each do |key|
-        d = Hash.new
-        d[:filesystem] = key[0]
-        d[:bytes] = key[1]
-        d[:used] = key[2]
-        d[:available] = key[3]
-        d[:use_percent] = key[4]
-        d[:mount] = key[5]
-        res.push(d) unless ['tmpfs', 'devtmpfs'].include? d[:filesystem]
-      end
-      res
-    end
-  end
+			mount = []
+			res = []
+			s.each do |line|
+				word = line.split(/\s+/)
+				mount.push(word)
+			end
+			mount.each do |key|
+				d = Hash.new
+				d[:filesystem] = key[0]
+				d[:bytes] = key[1]
+				d[:used] = key[2]
+				d[:available] = key[3]
+				d[:use_percent] = key[4]
+				d[:mount] = key[5]
+				res.push(d) unless ['tmpfs', 'devtmpfs'].include? d[:filesystem]
+			end
+			res
+		end
+	end
 end
