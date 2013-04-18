60592c7471e8824fd3ac37ec33f77197b017c92b
Carlos Puchol <cpg+git@amahi.org>
2013-04-17 18:14:13 -0700

    document the steps on how to add sample data for development

--- lib/sample_data.rb
+++ lib/sample_data.rb
@@ -1,4 +1,28 @@
-# class to load sample data
+# SampleData class to load sample data useful in covering many real-life cases in development
+# like hard drives temperature, disk size, etc., e.g. if developing on a system like mac os x
+#
+# Here is how to create fake data, say for Server datastructures
+#
+# on a real system with real data:
+#
+#   bash$ rails c
+#   :001 > s = Server.all
+#   => [#<Server ...>]
+#   :002 > open("servers.yml", 'w') {|f| f.write s.to_yaml}
+#
+# This will save a file called servers.yml. You can edit as necessary to cover corner cases.
+# Then do this:
+#
+#   gzip servers.yml
+#   mv servers.yml.gz db/sample-data/
+#
+# after a rails restart, you can then load the data in some controller or the console with
+#
+#   @servers = SampleData.load('servers')
+#
+# In this case, servers is a Rails model, so we need to require 'server' at the top of this file,
+# even though it creates extra load/dependencies/bloat to the app. FIXME: load only in development?
+#
 
 # require the server model here because we have some Server class data being loaded
 require 'server'
