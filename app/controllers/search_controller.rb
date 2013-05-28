572a36e61d618e4ba92dce555cf4846ffd51929b
Carlos Puchol <cpg+git@amahi.org>
2013-05-27 19:36:54 -0700

    add pagination and caching in the search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # Amahi Home Server
 # Copyright (C) 2007-2013 Amahi
 #
@@ -14,12 +15,16 @@
 # License along with this program; if not, write to the Amahi
 # team at http://www.amahi.org/ under "Contact Us."
 
+require 'digest/sha1'
+
 class SearchController < ApplicationController
 
 	before_filter :login_required
 	layout 'basic'
 
-	RESULTS_PER_PAGE = 60
+	RESULTS_PER_PAGE = 4
+
+	SEARCH_CACHE = File.join(Rails.root, 'tmp/cache/search')
 
 	EXT_AUDIO = ['aac', 'aif', 'iff', 'm3u', 'm4a', 'mid', 'midi', 'mp3', 'mpa', 'ra', 'ram', 'wav', 'wma']
 	EXT_IMAGES = ['mng', 'pct', 'bmp', 'gif', 'jpeg', 'jpg', 'png', 'psd', 'psp', 'thm', 'tif', 'ai', 'drw', 'dxf', 'eps', 'ps', 'svg', '3dm', '3dmf']
@@ -34,8 +39,10 @@ class SearchController < ApplicationController
 			redirect_to URI.escape("http://www.google.com/search?q=#{params[:query]}")
 		else
 			@query = params[:query]
-			unless use_sample_data?
-				@results = hda_search(@query)
+			@page = (params[:page] && params[:page].to_i.abs) || 1
+			@rpp = RESULTS_PER_PAGE
+			unless false && use_sample_data?
+				@results = hda_search(@query, nil, @page)
 			else
 				# NOTE: this is some sample fake data for development
 				@results = SampleData.load('search')
@@ -45,19 +52,25 @@ class SearchController < ApplicationController
 
 	def images
 		@query = params[:query]
-		@results = hda_search(@query, EXT_IMAGES)
+		@page = (params[:page] && params[:page].to_i.abs) || 1
+		@rpp = RESULTS_PER_PAGE
+		@results = hda_search(@query, EXT_IMAGES, @page)
 		render 'hda'
 	end
 
 	def audio
 		@query = params[:query]
-		@results = hda_search(@query, EXT_AUDIO)
+		@page = (params[:page] && params[:page].to_i.abs) || 1
+		@rpp = RESULTS_PER_PAGE
+		@results = hda_search(@query, EXT_AUDIO, @page)
 		render 'hda'
 	end
 
 	def video
 		@query = params[:query]
-		@results = hda_search(@query, EXT_VIDEO)
+		@page = (params[:page] && params[:page].to_i.abs) || 1
+		@rpp = RESULTS_PER_PAGE
+		@results = hda_search(@query, EXT_VIDEO, @page)
 		render 'hda'
 	end
 
@@ -66,29 +79,52 @@ class SearchController < ApplicationController
 
 protected
 
-	def hda_search(term, filter = nil)
-		res = locate_search(term)
-		return res unless filter
-		f = filter.join('|')
-		res.select { |r| r[:title] =~ /\.(#{f})$/ }
+	def hda_search(term, filter = nil, page)
+		return [] unless term && !term.blank?
+		locate_search(term, filter && filter.join('|'), page)
 	end
 
 	# FIXME: implement pagination
-	def locate_search(term)
-		# ignore case
-		case_sensitive = (term =~ /[A-Z]/) ? "" : "-i"
+	def locate_search(term, filter, page)
 		base = Share.basenames
-		open "| locate #{case_sensitive} -e #{term}" do |l|
+		# lines to skip
+		skip = (page - 1) * RESULTS_PER_PAGE
+		open locate_cache(term) do |l|
+			skipped = 0
 			res = []
-			while file = l.gets
+			while (file = l.gets) && res.size < RESULTS_PER_PAGE
 				file.strip!
 				path = pathname(file, base)
 				next unless path
 				r = locate2result(file, path)
-				res << r if r
-				# return res if (nresults -= 1) == 0
+				next unless r
+				next if filter && r[:title] !~ /\.(#{filter})$/
+				if skipped < skip
+					skipped += 1
+					next
+				end
+				res << r
 			end
 			res
+		end
+	end
+
+	def locate_cache(term)
+		FileUtils.mkdir_p(SEARCH_CACHE)
+		sha1 = Digest::SHA1.hexdigest(term)
+		cache = File.join(SEARCH_CACHE, sha1)
+		# expire old entries in the cache to prevent accumulation (12 hours, index is 24 hours old)
+		Dir.glob(File.join(SEARCH_CACHE, '*')) do |f|
+			FileUtils.rm_f(f) if Time.now - File.mtime(f) > 12.hours
+		end
+		# is the search already in the cache? if so, return it, if not make it
+		if File.exists?(cache)
+			cache
+		else
+			# ignore case unless the search is done with some capitalization
+			case_sensitive = (term =~ /[A-Z]/) ? "" : "-i"
+			system "locate #{case_sensitive} -e '#{term}' > #{cache}"
+			cache
 		end
 	end
 
@@ -112,6 +148,6 @@ protected
 
 	def pathname(file, basenames)
 		basenames.each { |b,name| return [name, $1] if file =~ /^#{b}(.*)/ }
-		return nil
+		nil
 	end
 end
