2e58b66e164c3475830d422f0f30cb15eba88d9f
Carlos Puchol <cpg+git@amahi.org>
2013-08-05 16:22:42 -0700

    servers area should be showing only when advanced settings are enabled

--- plugins/080-settings/config/initializers/plugin_init.rb
+++ plugins/080-settings/config/initializers/plugin_init.rb
@@ -2,5 +2,5 @@
 t = Tab.new("settings", "settings", "/tab/settings")
 # add any subtabs with what you need. params are controller and the label
 t.add("index", "details")
-t.add("servers", "servers")
+t.add("servers", "servers", true)
 t.add("themes", "themes")
