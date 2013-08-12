32cf91655528f9e3c2881f865f43970b0349e466
Carlos Puchol <cpg+git@amahi.org>
2013-08-11 17:26:48 -0700

    make the unzip quiet to make sure it does not disrupt app installs

--- app/models/app.rb
+++ app/models/app.rb
@@ -447,7 +447,7 @@ class App < ActiveRecord::Base
 
 	def unpack(url, fname)
 		if (url =~ /\.zip$/)
-			system("unzip #{fname}")
+			system("unzip -q #{fname}")
 		elsif (url =~ /\.(tar.gz|tgz)$/)
 			system("tar -xzf #{fname}")
 		elsif (url =~ /\.(tar.bz2)$/)
