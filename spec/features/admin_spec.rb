6ea1c648b790eb21657998482861ad35aa557172
MattHamada <mattahamada@gmail.com>
2014-03-09 11:51:56 -0700

    Added invalid admin login credential tests

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -2,6 +2,25 @@ require 'spec_helper'
 
 feature "Admin" do
 
+  scenario 'user should not be able to access pages without login' do
+    visit users_engine_path
+    page.should have_content("Amahi Server Login")
+    page.should have_content("You must be logged in to access this area")
+    visit shares_engine_path
+    page.should have_content("Amahi Server Login")
+    page.should have_content("You must be logged in to access this area")
+    visit disks_engine_path
+    page.should have_content("Amahi Server Login")
+    page.should have_content("You must be logged in to access this area")
+  end
+
+  scenario "user should be redirected to login page with unsuccessful login" do
+    visit root_path
+    click_button "Log In"
+    page.should have_content("Amahi Server Login")
+    page.should_not have_content("Logout")
+  end
+
 	scenario "user should see the setup pages after login" do
 		user = create(:admin)
 		visit root_path
