802542446b86932051ad80c41f76102ea7b7c2d1
Arpit Goyal <agmps17@gmail.com>
2014-03-09 19:40:09 +0530

    Somehow missed this, but has_text?, has_field? just return boolean, we need to have a condtion for a test to fail. Till now it was just returning boolean
    Signed-off-by: Arpit Goyal <agmps17@gmail.com>

--- spec/features/users_tab_spec.rb
+++ spec/features/users_tab_spec.rb
@@ -4,14 +4,14 @@ feature "Users tab" do
 	scenario "an admin should be able to create a new user", :js => true do
 		user = create(:admin)
 		visit root_path
-		page.has_text?("Amahi Server Login")
+		page.should have_content("Amahi Server Login")
 		fill_in "username", :with => user.login
 		fill_in "password", :with => "secret"
 		click_button "Log In"
-		page.has_text?("Setup")
+		page.should have_content("Setup")
 		visit users_engine.users_path
-		page.has_text?("Username")
-		page.has_text?("Full Name")
+		page.should have_content("Username")
+		page.should have_content("Full Name")
 		click_button "New User"
 		fill_in "user_login", :with => "newuser"
 		fill_in "user_name", :with => "fullname"
@@ -20,38 +20,38 @@ feature "Users tab" do
 		click_button "user_create_button"
 		wait_for_ajax
 		visit users_engine.users_path
-		page.has_text?("newuser")
-		page.has_text?("fullname")
+		page.should have_content("newuser")
+		page.should have_content("fullname")
 	end
 	scenario "should allow an admin user to delete a regular user", :js => true do
 		admin = create(:admin)
 		user = create(:user)
 		visit root_path
-		page.has_text?("Amahi Server Login")
+		page.should have_content("Amahi Server Login")
 		fill_in "username", :with => admin.login
 		fill_in "password", :with => "secret"
 		click_button "Log In"
-		page.has_text?("Setup")
+		page.should have_content("Setup")
 		visit users_engine.users_path
-		page.has_text?("Username")
-		page.has_text?("Full Name")
+		page.should have_content("Username")
+		page.should have_content("Full Name")
 		find("#whole_user_#{user.id}").find("tr").click_link user.login
 		expect(page).to have_selector("a#delete-user-#{user.id}", :visible => true)
 		click_link "delete-user-#{user.id}"
-		page.has_no_text?(user.name)
+		page.should have_no_content(user.name)
 	end
 	scenario "should not allow an admin user to delete its own account" do
 		admin = create(:admin)
 		user = create(:user)
 		visit root_path
-		page.has_text?("Amahi Server Login")
+		page.should have_content("Amahi Server Login")
 		fill_in "username", :with => admin.login
 		fill_in "password", :with => "secret"
 		click_button "Log In"
-		page.has_text?("Setup")
+		page.should have_content("Setup")
 		visit users_engine.users_path
-		page.has_text?("Username")
-		page.has_text?("Full Name")
+		page.should have_content("Username")
+		page.should have_content("Full Name")
 		find("#whole_user_#{admin.id}").find("tr").click_link admin.login
 		expect(page).to have_no_selector("a#delete-user-#{user.id}", :visible => true)
 	end
@@ -59,14 +59,14 @@ feature "Users tab" do
 		admin = create(:admin)
 		user = create(:user)
 		visit root_path
-		page.has_text?("Amahi Server Login")
+		page.should have_content("Amahi Server Login")
 		fill_in "username", :with => admin.login
 		fill_in "password", :with => "secret"
 		click_button "Log In"
-		page.has_text?("Setup")
+		page.should have_content("Setup")
 		visit users_engine.users_path
-		page.has_text?("Username")
-		page.has_text?("Full Name")
+		page.should have_content("Username")
+		page.should have_content("Full Name")
 		find("#whole_user_#{admin.id}").find("tr").click_link admin.login
 		expect(page.find_by_id("checkbox_user_admin_#{admin.id}")[:disabled]).to eq 'disabled'
 	end
@@ -74,14 +74,14 @@ feature "Users tab" do
 		admin = create(:admin)
 		user = create(:admin)
 		visit root_path
-		page.has_text?("Amahi Server Login")
+		page.should have_content("Amahi Server Login")
 		fill_in "username", :with => admin.login
 		fill_in "password", :with => "secret"
 		click_button "Log In"
-		page.has_text?("Setup")
+		page.should have_content("Setup")
 		visit users_engine.users_path
-		page.has_text?("Username")
-		page.has_text?("Full Name")
+		page.should have_content("Username")
+		page.should have_content("Full Name")
 		find("#whole_user_#{user.id}").find("tr").click_link user.login
 		checkbox = "checkbox_user_admin_#{user.id}"
 		page.should have_checked_field(checkbox)
@@ -94,18 +94,18 @@ feature "Users tab" do
 		admin = create(:admin)
 		user = create(:user)
 		visit root_path
-		page.has_text?("Amahi Server Login")
+		page.should have_content("Amahi Server Login")
 		fill_in "username", :with => admin.login
 		fill_in "password", :with => "secret"
 		click_button "Log In"
-		page.has_text?("Setup")
+		page.should have_content("Setup")
 		visit users_engine.users_path
-		page.has_text?("Username")
-		page.has_text?("Full Name")
+		page.should have_content("Username")
+		page.should have_content("Full Name")
 		find("#whole_user_#{user.id}").find("tr").click_link user.login
 		checkbox = "checkbox_user_admin_#{user.id}"
 		expect(user.admin?).to eq false
-		page.should_not have_checked_field(checkbox)
+		page.should have_no_checked_field(checkbox)
 		page.check(checkbox)
 		wait_for_ajax
 		page.should have_checked_field(checkbox)
@@ -114,73 +114,72 @@ feature "Users tab" do
 	scenario "should allow an admin user to change his full name", :js => true do
 		admin = create(:admin)
 		visit root_path
-		page.has_text?("Amahi Server Login")
+		page.should have_content("Amahi Server Login")
 		fill_in "username", :with => admin.login
 		fill_in "password", :with => "secret"
 		click_button "Log In"
-		page.has_text?("Setup")
+		page.should have_content("Setup")
 		visit users_engine.users_path
-		page.has_text?("Username")
-		page.has_text?("Full Name")
+		page.should have_content("Username")
+		page.should have_content("Full Name")
 		user_link = find("#whole_user_#{admin.id}")
 		user_link.find("tr").click_link admin.login
-		page.has_button?('Edit')
-		page.has_field?("name",:with=>"#{admin.name}")
+		page.should have_button('Edit')
+		page.should have_field("name",:with=>"#{admin.name}")
 		within("#form_user_#{admin.id}") do
 			fill_in "name" ,:with=>"changedname"
 			click_button "Edit"
 			wait_for_ajax
 		end
-		page.has_field?("name",:with=>"changedname")
-		user_link.has_field?("tr",:with=>"changedname")
+		page.should have_field("name",:with=>"changedname")
+		user_link.find("table.settings").should have_content("changedname")
 		expect(admin.reload.name).to eq "changedname"
 	end
 	scenario "should allow an admin user to change the full name of another user", :js => true do
 		admin = create(:admin)
 		user = create(:user)
 		visit root_path
-		page.has_text?("Amahi Server Login")
+		page.should have_content("Amahi Server Login")
 		fill_in "username", :with => admin.login
 		fill_in "password", :with => "secret"
 		click_button "Log In"
-		page.has_text?("Setup")
+		page.should have_content("Setup")
 		visit users_engine.users_path
-		page.has_text?("Username")
-		page.has_text?("Full Name")
+		page.should have_content("Username")
+		page.should have_content("Full Name")
 		user_link = find("#whole_user_#{user.id}")
 		user_link.find("tr").click_link user.login
-		page.has_button?('Edit')
-		page.has_field?("name",:with=>"#{user.name}")
+		page.should have_button('Edit')
+		page.should have_field("name",:with=>"#{user.name}")
 		within("#form_user_#{user.id}") do
 			fill_in "name" ,:with=>"changedname"
 			click_button "Edit"
 			wait_for_ajax
 		end
-		page.has_field?("name",:with=>"changedname")
-		user_link.has_field?("tr",:with=>"changedname")
+		page.should have_field("name",:with=>"changedname")
+		user_link.find("table.settings").should have_content("changedname")
 		expect(user.reload.name).to eq "changedname"
 	end
 	scenario "should allow an admin user to change his password" do
 		admin = create(:admin)
 		user = create(:user)
 		visit root_path
-		page.has_text?("Amahi Server Login")
+		page.should have_content("Amahi Server Login")
 		fill_in "username", :with => admin.login
 		fill_in "password", :with => "secret"
 		click_button "Log In"
-		page.has_text?("Setup")
+		page.should have_content("Setup")
 		visit users_engine.users_path
-		page.has_text?("Username")
-		page.has_text?("Full Name")
+		page.should have_content("Username")
+		page.should have_content("Full Name")
 		user_link = find("#whole_user_#{admin.id}")
 		user_link.find("tr").click_link admin.login
 		within(user_link) do
 			expect(user_link).to have_selector("a#user-password-control-action-#{admin.id}", :visible => true)
-			user_link.has_link?("a#user-password-control-action-#{admin.id}")
 			link = user_link.find_by_id("user-password-control-action-#{admin.id}")
 			link.click
-			user_link.has_field?("user[password]")
-			user_link.has_field?("user[password_confirmation]")
+			user_link.should have_field("user[password]")
+			user_link.should have_field("user[password_confirmation]")
 			password_input = user_link.find_field("user[password]")
 			password_confirm_input = user_link.find_field("user[password_confirmation]")
 			password_input.set "secret"
@@ -195,23 +194,22 @@ feature "Users tab" do
 		admin = create(:admin)
 		user = create(:user)
 		visit root_path
-		page.has_text?("Amahi Server Login")
+		page.should have_content("Amahi Server Login")
 		fill_in "username", :with => admin.login
 		fill_in "password", :with => "secret"
 		click_button "Log In"
-		page.has_text?("Setup")
+		page.should have_content("Setup")
 		visit users_engine.users_path
-		page.has_text?("Username")
-		page.has_text?("Full Name")
+		page.should have_content("Username")
+		page.should have_content("Full Name")
 		user_link = find("#whole_user_#{user.id}")
 		user_link.find("tr").click_link user.login
 		within(user_link) do
 			expect(user_link).to have_selector("a#user-password-control-action-#{user.id}", :visible => true)
-			user_link.has_link?("a#user-password-control-action-#{user.id}")
 			link = user_link.find_by_id("user-password-control-action-#{user.id}")
 			link.click
-			user_link.has_field?("user[password]")
-			user_link.has_field?("user[password_confirmation]")
+			user_link.should have_field("user[password]")
+			user_link.should have_field("user[password_confirmation]")
 			password_input = user_link.find_field("user[password]")
 			password_confirm_input = user_link.find_field("user[password_confirmation]")
 			password_input.set "secret"
