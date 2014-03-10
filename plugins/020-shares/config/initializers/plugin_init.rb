939d4e6a3825cec429b0bf2b4fc047f97f93a6cf
Arpit Goyal <agmps17@gmail.com>
2014-03-10 18:06:32 +0530

    Adding settings to an advanced tab

--- plugins/020-shares/config/initializers/plugin_init.rb
+++ plugins/020-shares/config/initializers/plugin_init.rb
@@ -2,5 +2,5 @@
 t = Tab.new("shares", "shares", "/tab/shares")
 # add any subtabs with what you need. params are controller and the label, for example
 t.add("index", "details")
-# subtab for workgroup settings
-t.add("settings", "settings")
+#Advanced Tab, workgroup settings
+t.add("settings", "settings",true)
