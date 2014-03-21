f5fd610124ef72de893a9d0cfec08743821026b2
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-03-21 12:40:59 +0530

    Removed test for system_all_new_user method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,11 +23,4 @@ describe User do
 		User.system_find_name_by_username(username).should include(`cat /etc/passwd | grep `+username+` | cut -d: -f5`.gsub(/\,/,"").strip
 )
 	end
-
-	it "should find all new users" do
-		v =`cd home; cat /etc/passwd | grep "/home" | grep "/bin/bash" | cut -d: -f1 | wc -l`.strip.to_i
-		puts "#{User.system_all_new_users.count}"
-		puts "#{`cat /etc/passwd | grep "/home" | grep "/bin/bash" | cut -d: -f1`}"
-		User.system_all_new_users.count.should == v
-	end
 end
