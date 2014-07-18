040b9f86dd5d2a222d929ede39cf27b5ebf5c412
Arpit Goyal <agmps17@gmail.com>
2014-07-18 11:57:20 +0530

    Adding tag 'tv' to teh existing tv share
    Signed-off-by: Arpit Goyal <agmps17@gmail.com>

--- db/migrate/20140717200213_add_tv_to_shares.rb
+++ db/migrate/20140717200213_add_tv_to_shares.rb
@@ -3,7 +3,12 @@ class AddTvToShares < ActiveRecord::Migration
 		if(Setting.get('initialized') && Setting.get('initialized') == '1')
 			name = "TV"
 			sh = Share.where(:name=>name).first
-			if !sh
+			if sh
+				if !sh.tags.include?("tv")
+					sh.tags = "#{sh.tags}, tv"
+					sh.save!
+				end
+			else
 				sh = Share.new
 				sh.path = Share.default_full_path(name)
 				sh.name = name
