f51f05e5debf81a1ec0ae843058bd75c04ebbce6
Arpit Goyal <agmps17@gmail.com>
2014-08-04 00:53:28 +0530

    Changing waiting time when installation is going on.
    Signed-off-by: Arpit Goyal <agmps17@gmail.com>

--- lib/daemons/app_installation.rb
+++ lib/daemons/app_installation.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 # You might want to change this
-ENV["RAILS_ENV"] ||= "development" #change to production in production
+ENV["RAILS_ENV"] ||= "production"
 
 root = File.expand_path(File.dirname(__FILE__))
 root = File.dirname(root) until File.exists?(File.join(root, 'config'))
@@ -11,7 +11,7 @@ require File.join(root, "config", "environment")
 
 
 $running = true
-Signal.trap("EXIT") do
+Signal.trap("TERM") do
   $running = false
   Rails.logger.info ("died")
 end
@@ -23,16 +23,19 @@ AmahiApi::api_key = Setting.value_by_name("api-key")
 
 while $running do
   # Replace this with your code
-  	identifier = Setting.value_by_name("app_api")#Right now using the db value
-  	#identifier = AmahiApi::AppToInstall
+    identifier = Setting.value_by_name("app_api")#Right now using the db value
+    #identifier = AmahiApi::AppToInstall
 
-  	#App instalaltion starts if there is some app to install
-  	if !identifier.nil?
-  		@app = App.where(:identifier=>identifier).first
-		App.install identifier unless @app
-		Setting.set("app_api",nil) if Rails.env == "development"
-		sleep(40)
-	else
-		sleep(10)
-	end
+    #App instalaltion starts if there is some app to install
+    if !identifier.nil?
+      @app = App.where(:identifier=>identifier).first
+    App.install identifier unless @app
+    File.open('log/app_installation.rb.log','a') do |f|
+      f.write('App installation of '+identifier+"started  at #{Time.now}\n")
+    end
+    Setting.set("app_api",nil) #if Rails.env == "development"
+    sleep(180)
+  else
+    sleep(10)
+  end
 end
