b566eba3b8e967e5a71cde67fcb55f9a78835931
Arpit Goyal <agmps17@gmail.com>
2014-08-05 00:51:35 +0530

    Test if daemon is already initialized to prevent looping

---
+++ config/daemon.rb
@@ -0,0 +1,6 @@
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+# Initialize the rails application
