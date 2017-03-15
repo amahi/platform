ad7f3962771f163c03235bcfde784a8d1050d287
MajidSas <majid.saeedan@gmail.com>
2017-03-15 04:01:36 +0300

    removes lines of commented deprecated code from factories.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -10,10 +10,7 @@ FactoryGirl.define do
 		# we do not want to create users in the system
 		before(:create) do |u|
 			allow(u).to receive(:before_create_hook) { nil }
-			# u.stub(:before_create_hook) { nil }
-			# u.stub(:after_save_hook) { nil }
 			allow(u).to receive(:after_save_hook) { nil }
-			# allow(u).to receive(:after_create_hook) { nil }
 		end
 
 		# an admin user
