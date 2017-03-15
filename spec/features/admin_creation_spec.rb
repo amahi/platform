663e2e12790629f6fc54a5f469a91dc47e8ae71f
Majid Saeedan <majid.saeedan@gmail.com>
2017-03-15 12:22:35 +0300

    Deprecation Fixes on Rails 4.2.6 and Updating RSpec Syntax to RSpec 3 (#127)
    various fixes in tests and some small change in deprecation
    
    * fixes trivial deprecations
    
    * Convert specs to RSpec 3.4.4 syntax with Transpec
    
    This conversion is done by Transpec 3.3.0 with the following command:
        transpec
    
    * 90 conversions
        from: obj.should
          to: expect(obj).to
    
    * 9 conversions
        from: == expected
          to: eq(expected)
    
    * 4 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    * 1 conversion
        from: obj.stub(:message)
          to: allow(obj).to receive(:message)
    
    * 1 addition
          of: RSpec.configure { |c| c.infer_spec_type_from_file_location! }
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions
    
    * updated stub in factory girl to new syntax
    
    * removes lines of commented deprecated code from factories.rb
    
    * fixes raise_error warning to avoid false positives by adding the expected type of error
    
    * fixes auto generated tests for checkbox from equals to 'disabled' to equals to true
    
    * fixes deprecated variable name on auto generated file by transpec
    
    * fixes admin password and user for capybara tests
    
    * remove unnecessary obsolete configs
    
    config.active_record.whitelist_attributes

--- spec/features/admin_creation_spec.rb
+++ spec/features/admin_creation_spec.rb
@@ -10,15 +10,15 @@ feature "Admin creation" do
 	scenario "first login for admin should see the setup page and setup first admin" do
 		(s = Setting.where(:name=>'initialized').first) && s.destroy
 		username = "newuser"
-		User.stub(:system_find_name_by_username) { ["New User", 1000, username] }
+		allow(User).to receive(:system_find_name_by_username) { ["New User", 1000, username] }
 		visit start_path
-		page.should have_content("Amahi initialization")
+		expect(page).to have_content("Amahi initialization")
 		fill_in "username", :with => username
 		fill_in "password", :with => "secret"
 		fill_in "password_confirmation", :with => "secret"
 		click_button "Create"
 		user = User.where(:login => username).first
-		user.admin.should be_truthy
+		expect(user.admin).to be_truthy
 	end
 
 end
--- spec/support/wait_for_ajax.rb
+++ spec/support/wait_for_ajax.rb
@@ -1,6 +1,6 @@
 module WaitForAjax
 	def wait_for_ajax
-	  Timeout.timeout(Capybara.default_wait_time) do
+	  Timeout.timeout(Capybara.default_max_wait_time) do
 	    loop do
 	      active = page.evaluate_script('jQuery.active')
 	      break if active == 0
@@ -11,4 +11,15 @@ end
 
 RSpec.configure do |config|
   config.include WaitForAjax, type: :feature
+
+  # rspec-rails 3 will no longer automatically infer an example group's spec type
+  # from the file location. You can explicitly opt-in to the feature using this
+  # config option.
+  # To explicitly tag specs without using automatic inference, set the `:type`
+  # metadata manually:
+  #
+  #     describe ThingsController, :type => :controller do
+  #       # Equivalent to being in spec/controllers
+  #     end
+  config.infer_spec_type_from_file_location!
 end
