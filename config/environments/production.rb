53c5c43a0074d63f29fd4ce34bc4d0f83a3973f4
sukhbir-singh <sukhbir947@gmail.com>
2018-07-04 04:10:32 +0530

    set assets debug in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   config.assets.compile = true
   config.serve_static_assets = true
 
+  config.assets.debug = true
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
