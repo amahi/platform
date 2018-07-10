6419b9d8e14645b40cee720b673889115a4e8fcf
Sukhbir Singh <sukhbir947@gmail.com>
2018-07-10 05:44:16 +0530

    fixes assets missing bug for plugins (#209)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.precompile += %w( login.css )
+
+Rails.application.config.assets.precompile += Dir[Rails.root.join('plugins', '**' , '[^_]*.{js,css,sass,coffee}')]
